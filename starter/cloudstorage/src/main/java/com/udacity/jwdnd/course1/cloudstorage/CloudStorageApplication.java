2612348734f3612d37980da4543d1b41ece00027
tranngocdinhkhanh <tranngocdinhkhanh@gmail.com>
2024-08-16 15:40:44 +0700

    choi ngu

---
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/CloudStorageApplication.java
@@ -0,0 +1,14 @@
+package com.udacity.jwdnd.course1.cloudstorage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudStorageApplication {
+
+	public static void main(String[] args) {
+		// demo git
+		SpringApplication.run(CloudStorageApplication.class, args);
+	}
+
+}
